1547c7521a3684323cb1b7e19b428365e9d66ba4
Memo Chou <memochou1993@gmail.com>
2022-07-16 16:20:02 +0800

    Add demo page

---
+++ src/main.js
@@ -0,0 +1,67 @@
+import { ethers } from 'ethers';
+import { abi } from '../build/contracts/TodoList.json';
+
+const CONTRACT_ADDRESS = '0xB15DcE2F04b6C85918EcC46015c4F4d2A0cBf504';
+
+class App {
+  constructor() {
+    this.init();
+  }
+
+  async init() {
+    await this.loadContract();
+    await this.renderTasks();
+
+    document.querySelector('button').addEventListener('click', (e) => this.createTask(e));
+  }
+
+  async loadContract() {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    await provider.send('eth_requestAccounts');
+    const signer = provider.getSigner();
+    this.contract = new ethers.Contract(CONTRACT_ADDRESS, abi, signer);
+  }
+
+  async renderTasks() {
+    const taskCount = await this.contract.taskCount();
+    const list = document.getElementById('list');
+    list.textContent = '';
+    for (let i = 0; i < taskCount; i++) {
+      const task = await this.contract.tasks(i);
+      const [idx, content, completed] = task;
+      const checkbox = document.createElement('input');
+      checkbox.type = 'checkbox';
+      checkbox.name = idx;
+      checkbox.checked = completed;
+      checkbox.addEventListener('click', (e) => this.updateTask(e));
+      const item = document.createElement('li');
+      item.textContent = content;
+      item.prepend(checkbox);
+      list.append(item);
+    }
+  }
+
+  async createTask() {
+    try {
+      const content = document.getElementById('content');
+      const res = await this.contract.createTask(content.value);
+      await res.wait();
+      await this.renderTasks();
+      content.value = '';
+    } catch (err) {
+      alert(err.message);
+    }
+  }
+
+  async updateTask(e) {
+    try {
+      const res = await this.contract.updateTask(e.target.name, e.target.checked);
+      await res.wait();
+    } catch (err) {
+      alert(err.message);
+      e.target.checked = !e.target.checked;
+    }
+  }
+}
+
+window.onload = () => new App();
